f77772cdb1f51a1054a96eefe01d15d32d427f7c
Tanmoy Sikder <tanmoysikder39@gmail.com>
2022-03-02 15:09:15 +0600

    load more

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [item, setItem] = useState([]);
+  const [visible, setVisible] = useState(3);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => {
+        setItem(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const showMoreItem = () => {
+    setVisible((prevValue) => prevValue + 3);
+  };
+  return (
+    <div className="App">
+      {item.slice(0, visible).map((item) => (
+        <div className="card">
+          <div className="id">
+            <span>{item.id}</span>
+          </div>
+          <p>{item.title}</p>
+          <p>{item.setItem}</p>
+          <img src={item.thumbnailUrl} alt="" />
+        </div>
+      ))}
+      <button onClick={showMoreItem}>Load More</button>
+    </div>
+  );
+}
+
+export default App;
